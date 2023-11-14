253d3fd5d7f200ed0d49b038b9f481c251324df9
Alexander Bessman <alexander.bessman@scania.com>
2023-11-14 10:28:55 +0100

    Stay in bootloader if application returns

--- src/main.c
+++ src/main.c
@@ -6,10 +6,10 @@
  * (RC5) is grounded, the device will stay in bootloader mode even if an
  * application exists. While in bootloader mode, a new application can be
  * flashed with the Unified Bootloader Host Application, or a similar tool.
- * 
+ *
  * From PSLab V6 revision onwards, there is a push button attached to BOOT pin
  * with a label `BOOT`. Holding this button down at power up or when clicking on
- * `Read Device Settings` in Unified Bootloader application will switch from  
+ * `Read Device Settings` in Unified Bootloader application will switch from
  * main application to bootloader mode where one can flash a new firmware.
  */
 
@@ -41,16 +41,14 @@ int main(void) {
     // If BOOT is grounded or no application is detected, stay in bootloader.
     if (BOOT_PIN_GetValue() && BOOT_Verify()) {
         BOOT_StartApplication();
-    } else {
-        uint16_t i = 0;
+    }
 
-        while (1) {
-            // Monitor serial bus for commands, e.g. flashing new application.
-            BOOT_ProcessCommand();
+    for (uint16_t i = 0; 1; ++i) {
+        // Monitor serial bus for commands, e.g. flashing new application.
+        BOOT_ProcessCommand();
 
-            // Blink system LED while in bootloader mode.
-            if (!i++) STATUS_LED_Toggle();
-        }
+        // Blink system LED while in bootloader mode.
+        if (!i) STATUS_LED_Toggle();
     }
 
     return 1;
